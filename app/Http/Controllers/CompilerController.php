1a54f75f40c35539ddc8dc318de265a936add296
Theofilusarifin <theofilusarifin@gmail.com>
2022-12-28 12:12:27 +0700

    Initial Files

---
+++ app/Http/Controllers/CompilerController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CompilerController extends Controller
+{
+    public function getCodde(Request $request){
+        dd($request);
+    }
+}
